906d37a19ce437ab7c0676299eddd388b9de1ba6
unknown <u3shadow@gmail.com>
2016-03-18 11:50:27 +0800

    finish base functions

--- app/src/main/java/com/u3coding/dashboard/MainActivity.java
+++ app/src/main/java/com/u3coding/dashboard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.u3coding.dashboard;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyProgressBar progressBar = (MyProgressBar)findViewById(R.id.progress);
-        progressBar.setRadiu(50);
+        progressBar.setProgressColor(Color.RED);
+        progressBar.setProgressWidth(10);
+        progressBar.start(3);
+
     }
+
 }
--- app/src/main/java/com/u3coding/dashboard/MyProgressBar.java
+++ app/src/main/java/com/u3coding/dashboard/MyProgressBar.java
@@ -5,13 +5,23 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.CountDownTimer;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
 
 /**
  * Created by ${U3} on 2016/3/17.
  */
 public class MyProgressBar extends View{
+    float progress = 360;
+    int totalTime;
+    int boundWidth = 5;
+    private int progressWidth = 30;
+    private int progressColor = Color.GREEN;
+
+
     public MyProgressBar(Context context) {
         super(context);
     }
@@ -30,6 +40,41 @@ public class MyProgressBar extends View{
         this.radiu = radiu;
         invalidate();
     }
+    public void start(long time) {
+        time *= 60000;
+        final float step = (float)360/(time/30);
+        CountDownTimer mTimer = new CountDownTimer(time,30) {
+            public void onTick(long millisUntilFinished) {
+                 progress -= step;
+                invalidate();
+            }
+
+            @Override
+            public void onFinish() {
+                end(step);
+            }
+
+        };
+        mTimer.start();
+    }
+    private void end(float step){
+        progress -= step;
+        invalidate();
+        progress = 0;
+        invalidate();
+    }
+    public void setBoundWidth(int width){
+        boundWidth = width;
+        invalidate();
+    }
+    public void setProgressWidth(int width){
+        progressWidth = width;
+        invalidate();
+    }
+    public void setProgressColor(int color){
+        progressColor = color;
+        invalidate();
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -39,21 +84,20 @@ public class MyProgressBar extends View{
         final int paddingBottom = getPaddingBottom();
         int width = getWidth() - paddingLeft - paddingRight;
         int height = getHeight() - paddingTop - paddingBottom;
-        radiu = Math.min(width, height) / 2-5;
+        radiu = Math.min(width, height) / 2-boundWidth;
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
+        paint.setStrokeWidth(boundWidth);
         paint.setColor(Color.BLACK);
         canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radiu, paint);
-        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radiu - 30, paint);
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(30);
+        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radiu - progressWidth, paint);
+        paint.setColor(progressColor);
+        paint.setStrokeWidth(progressWidth);
         paint.setStrokeCap(Paint.Cap.ROUND);
-        RectF oval=new RectF();                     //RectF对象
-        oval.left=paddingLeft+15+5;                              //左边
-        oval.top=height / 2 - width/2+15+5;                                   //上边
-        oval.right=2*radiu+paddingLeft-15+5;                             //右边
-        oval.bottom=2*radiu+height / 2 - width/2-15+5;
-        canvas.drawArc(oval,360,360,false,paint);
+        RectF oval=new RectF();
+        oval.left=paddingLeft+progressWidth/2+boundWidth;
+        oval.top=height / 2 - width/2+progressWidth/2+boundWidth;
+        oval.right=2*radiu+paddingLeft-progressWidth/2+boundWidth;
+        oval.bottom=2*radiu + height / 2 - width / 2 - progressWidth/2 + boundWidth;
+        canvas.drawArc(oval,270,progress,false,paint);
     }
 }
